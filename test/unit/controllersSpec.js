7f4cbd169e14aea225b656e8772e827df37c48c9
Taylor Huston <taylor.a.huston@gmail.com>
2015-12-22 22:29:31 -0900

    Complete step 2 of tutorial

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -9,3 +9,24 @@ describe('controllers', function() {
   });
 
 });
+
+describe('PhonelistCtrl', function() {
+  
+  beforeEach(module('phonecatApp'));
+             
+  it('should create "phones" model with 3 phones', inject(function($controller) {
+    var scope = {},
+        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    
+    expect(scope.phones.length).toBe(3);
+  }));
+  
+  it('name hould be "Taylor"', inject(function($controller) {
+    var scope = {},
+        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    
+    expect(scope.name).toBe("Taylor");
+    
+  }));
+  
+});
